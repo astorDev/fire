edac00d5a07cb1eaf3369a263e8efe08fe12a10e
Egor Tarasov <pavluchenk0@yandex.ru>
2024-11-05 11:23:30 +0400

    more fluent configuration (#25)

--- Astor.Fireblocks.Client/FireblocksClientOptions.cs
+++ Astor.Fireblocks.Client/FireblocksClientOptions.cs
@@ -1,16 +1,15 @@
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
 namespace Astor.Fireblocks.Client;
 
 public record FireblocksClientOptions
 {
     [Required]
-    public string Url { get; set; }
+    public required string Url { get; set; }
 
     [Required]
-    public string ApiKey { get; set; }
+    public required string ApiKey { get; set; }
 
     [Required]
-    public string ApiSecret { get; set; }
+    public required string ApiSecret { get; set; }
 }
